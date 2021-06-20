6d438e452a0a9f7ab1debc03b9f4dcc968440ae4
Harshad Hatte <harshadhatte@gmail.com>
2021-06-20 12:42:33 +0530

    day 3

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Harshad";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fitness App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome to my app day $days with $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
-    runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    ;
+
     return MaterialApp(
-      home: Material(
-          child: Center(
-            child: Container(
-              child: Text("welcome to my app"),),
-          ),
-      ),
-     );
+      home: HomePage(),
+    );
   }
 }
