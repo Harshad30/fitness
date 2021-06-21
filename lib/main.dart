5cd3a9ce4b4b17a06b65b6104d9545df06935987
Harshad Hatte <harshadhatte@gmail.com>
2021-06-21 14:47:41 +0530

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:exercise_01/pages/login_Page.dart';
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +10,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    ;
-
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.purple), //Themedata
+      routes: {
+        "/": (context) => LoginPage(),
+        "/Login": (context) => HomePage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Fitness App"),
+        title: Center(child: Text("Fitness App")),
       ),
       body: Center(
         child: Container(
